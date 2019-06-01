a62c98b2ced09e6d3ab3f665f56eb56ce33d9547
CloudRex <cloudrex@outlook.com>
2019-05-31 21:58:36 -0400

    Remove default command

--- src/commands/luck.ts
+++
@@ -1,9 +0,0 @@
-import {Command, name, description, Context, Util} from "d.mix";
-
-@name("luck")
-@description("Determine your luck")
-export default class extends Command {
-    public async run($: Context): Promise<void> {
-        await $.send(`Your luck is ${Util.hash($.sender.id, 100)}%!`);
-    }
-}
